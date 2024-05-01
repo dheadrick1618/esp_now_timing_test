fc4c3beaa64009791a5bec26ad482e9e2a10099c
Devin <headrick@ualberta.ca>
2024-05-01 00:31:32 -0600

    Setup broadcast peer. Data buffer hardcoded with dummy data and esp now send is broadcasting periodically

--- broadcaster/main/broadcaster.c
+++ broadcaster/main/broadcaster.c
@@ -1,13 +1,23 @@
+/*
+Written by Devin Headrick
+
+This is broadcaster code to run on an esp32, initialized as a wifi 'station' interface.
+
+References:
+    - https://github.com/espressif/esp-idf/tree/master/examples/wifi/espnow
+    - https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/network/esp_now.html
+    - https://github.com/espressif/esp-idf/tree/master/examples/wifi/espnow
+*/
+
+#include <string.h> //for memcpy and memset
 #include <stdio.h>
 #include <freertos/FreeRTOS.h>
 #include "freertos/task.h"
-
 #define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
 #include "esp_log.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "nvs_flash.h" // I think the WiFi lib needs nvs flash to store wifi credentials
-
 #include "esp_now.h"
 
 #define DELAY(ms) vTaskDelay(pdMS_TO_TICKS(ms))
@@ -16,6 +26,10 @@
 static void esp_now_broadcast_task(void *pvParameter);
 
 static const char *TAG = "esp_broadcaster";
+
+static uint8_t esp_now_broadcast_mac[ESP_NOW_ETH_ALEN] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+const uint8_t data_buffer[] = {'E', 'S', 'P'};
 
 static void init_app_wifi(void)
 {
@@ -32,7 +46,7 @@ static void init_app_wifi(void)
 
 static void espnow_broadcast_send_cb(const uint8_t *mac_addr, esp_now_send_status_t status)
 {
-    printf("broadcast send callback fired \n");
+    // ESP_LOGI(TAG, "Broadcaster send callback fired");
 }
 
 /// @brief  Task that loops, constantly broadcasting data to any listening devices
@@ -42,12 +56,16 @@ static void esp_now_broadcast_task(void *pvParameter)
 
     for (;;)
     {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-        printf("Sending broadcast data\n");
+        ESP_LOGI(TAG, "Broadcasting espnow packet");
+        ESP_ERROR_CHECK(esp_now_send(esp_now_broadcast_mac, data_buffer, sizeof(data_buffer)));
+        DELAY_1S;
     }
 }
 
+/// @brief  Initialize espnow, register send callback, add broadcast peer into to peer list, and create
+///         task for esp now broadcast loop.
+/// @param  void
+/// @return esp_err_t
 static esp_err_t espnow_init(void)
 {
 
@@ -55,6 +73,21 @@ static esp_err_t espnow_init(void)
 
     // register a callback function - this is fired upon calling the esp_now_send() fxn
     ESP_ERROR_CHECK(esp_now_register_send_cb(espnow_broadcast_send_cb));
+
+    /* Add broadcast peer information to peer list. */
+    esp_now_peer_info_t *peer = malloc(sizeof(esp_now_peer_info_t));
+    if (peer == NULL)
+    {
+        ESP_LOGE(TAG, "Malloc peer information fail");
+        return ESP_FAIL;
+    }
+    memset(peer, 0, sizeof(esp_now_peer_info_t));
+    peer->channel = 0;
+    peer->ifidx = ESP_IF_WIFI_STA;
+    peer->encrypt = false;
+    memcpy(peer->peer_addr, esp_now_broadcast_mac, ESP_NOW_ETH_ALEN);
+    ESP_ERROR_CHECK(esp_now_add_peer(peer));
+    free(peer);
 
     xTaskCreate(
         esp_now_broadcast_task,
@@ -85,12 +118,4 @@ void app_main(void)
 
     // init esp now
     espnow_init();
-
-    // create task for esp now broadcast
-
-    while (1)
-    {
-        ESP_LOGE(TAG, "hello esp :) ");
-        DELAY_1S;
-    }
 }
